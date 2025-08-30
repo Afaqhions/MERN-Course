5a91d949c17c30d23ca4f669dbe13388690beecb
Muhammad Afaq Ahmad <m.afaqpak@gmail.com>
2025-08-30 10:55:49 +0500

    Update dbConnection.js

--- Fullstack/Backend/lib/dbConnection.js
+++ Fullstack/Backend/lib/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb+srv://mafaqpak:hack@p-1.k6vu1rr.mongodb.net/schoolDB');
+        await mongoose.connect('/schoolDB');
         console.log("database connected");
     } catch (error) {
         console.log("error", error);
